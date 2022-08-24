110e6ff030ebfda8a9c2b683044417ec931aec7c
diana-atas <dianadavidatas@gmail.com>
2022-08-24 12:32:38 +1200

    show history implement time travel

--- src/index.js
+++ src/index.js
@@ -45,17 +45,19 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props)
+    // initialise game's state
     this.state = {
       history: [
         {
           squares: Array(9).fill(null),
         },
       ],
+      stepNumber: 0,
       xIsNext: true, // sets first move to be 'X' by default
     }
   }
   handleClick(i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
 
@@ -65,16 +67,34 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O'
     this.setState({
       history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     })
     // console.log('history:', history)
     // console.log('current:', current)
     // console.log('squares:', squares)
   }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0, // set to true if step is even
+    })
+  }
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start'
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
+
     let status
     if (winner) {
       status = 'Winner: ' + winner
@@ -91,7 +111,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     )
